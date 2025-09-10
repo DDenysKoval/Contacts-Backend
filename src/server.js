1de3c16669de540c3b5bd1d293d02f9987569b43
Denys Koval <metalheadden@gmail.com>
2025-09-10 22:25:16 +0300

    hw2-01

--- src/server.js
+++ src/server.js
@@ -1,15 +1,16 @@
 import express from "express";
-import dotenv from "dotenv";
 import pino from 'pino-http';
 import cors from 'cors';
+import { getAllContacts } from "./servises/contacts.js";
 
-dotenv.config();
 const PORT = Number(process.env.PORT);
 
 const setupServer = () => {
 
   const app = express();
 
+  app.use(express.json());
+  app.use(cors());
   app.use(
     pino({
       transport: {
@@ -18,11 +19,34 @@ const setupServer = () => {
     })
   );
 
-  app.use(cors());
-
-  app.use("*", (req, res, next) => {
+  app.use((req, res, next) => {
     res.status(404).json({
       message: "Not found"
+    });
+  });
+
+  app.get("/contacts", async (res, req) => {
+    const contacts = await getAllContacts();
+    res.json({
+      status: 200,
+      message: "Successfully found contacts!",
+      data: contacts,
+    });
+  });
+
+  app.get("/contacts/:contactId", async (res, req, next) => {
+    const {contactId} = req.params;
+    const contact = await getAllContacts(contactId);
+    if (!contact) {
+      res.json({
+        message: 'Contact not found',
+      });
+      return;
+    }
+    res.json({
+      status: 200,
+      message: `Successfully found contact with id ${contactId}!`,
+      data: contact,
     });
   });
 
