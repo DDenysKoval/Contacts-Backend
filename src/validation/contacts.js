55550ea48850599763d0f5b53a51b69261c7d747
Denys Koval <metalheadden@gmail.com>
2025-09-29 22:28:43 +0300

    hw4-05

--- src/validation/contacts.js
+++ src/validation/contacts.js
@@ -3,7 +3,7 @@ import Joi from 'joi';
 export const createContactSchema = Joi.object({
   name: Joi.string().min(3).max(20).required(),
   phoneNumber: Joi.string().required(),
-  email: Joi.string().min(3).max(20),
+  email: Joi.string().email(),
   isFavorite: Joi.boolean(),
   contactType: Joi.string().valid('work', 'home', 'personal').required(),
 });
