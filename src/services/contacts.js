1b64a47db997cb2d218e9deb457507f3b0fd3d20
Denys Koval <metalheadden@gmail.com>
2025-09-29 21:53:52 +0300

    hw4-03

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -4,30 +4,30 @@ import { calculatePaginationData } from '../utils/calculatePaginationData.js';
 export const getAllContacts = async ({
   page,
   perPage,
-  sortBy,
-  sortOrder,
-  filter,
+  sortBy = 'asc',
+  sortOrder = '_id',
+  filter = {},
 }) => {
   const skip = page > 0 ? (page - 1) * perPage : 0;
   const contactQuery = ContactsCollection.find();
 
-  if (filter.contactType) {
+  if (typeof filter.contactType !== 'undefined') {
     contactQuery.where('contactType').equals(filter.contactType);
   }
 
-  if (filter.isFavourite) {
+  if (typeof filter.isFavourite !== 'undefined') {
     contactQuery.where('isFavourite').equals(filter.isFavourite);
   }
 
-  const [contacts, totalItems] = await Promise.all([
+  const [total, contacts] = await Promise.all([
+    ContactsCollection.find().merge(contactQuery).countDocuments(),
     contactQuery
       .sort({ [sortBy]: sortOrder })
       .skip(skip)
       .limit(perPage),
-    ContactsCollection.find().merge(contactQuery).countDocuments(),
   ]);
 
-  const paginationData = calculatePaginationData(totalItems, page, perPage);
+  const paginationData = calculatePaginationData(total, page, perPage);
 
   return {
     data: contacts,
--- src/utils/calculatePaginationData.js
+++ src/utils/calculatePaginationData.js
@@ -1,9 +1,7 @@
 export const calculatePaginationData = (count, page, perPage) => {
-  // console.log(count, perPage);
-
   const totalPages = Math.ceil(count / perPage);
   const hasPreviousPage = page !== 1;
-  const hasNextPage = Boolean(totalPages - page);
+  const hasNextPage = page < totalPages;
 
   return {
     page,
